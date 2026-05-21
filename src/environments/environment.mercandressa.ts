f539fe239a36d0d623ec7b157bbc690536fd29de
Thalles Henrique <thalles7765@users.noreply.github.com>
2026-05-21 10:34:51 -0300

    Add Mercandressa flavor

---
+++ src/environments/environment.mercandressa.ts
@@ -0,0 +1,27 @@
+export const environment = {
+  production: false,
+  flavor: "mercandressa",
+  appName: "Mercandressa App",
+  companyName: "Mercandressa",
+  companyFullName: "Mercandressa",
+  website: "",
+  assets: {
+    logo: "assets/brands/mercandressa/logo.png",
+    menuLogo: "assets/brands/mercandressa/Splash/img432xRect.png",
+    favicon: "assets/brands/mercandressa/icon/favicon.png",
+    banners: [
+      "assets/brands/mercandressa/banners/1.png",
+      "assets/brands/mercandressa/banners/2.png",
+      "assets/brands/mercandressa/banners/3.png",
+      "assets/brands/mercandressa/banners/4.png"
+    ]
+  },
+  theme: {
+    "--ion-color-primary": "#0f766e",
+    "--ion-color-secondary": "#334155",
+    "--ion-color-tertiary": "#f59e0b",
+    "--app-header-background": "#ffffff",
+    "--app-border-color": "#0f766e"
+  },
+  url_api: "http://85.31.63.163:5120/api/v1"
+};
